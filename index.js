9c7220bf8176a57cba14b3b233783229eb1787a5
rainbowmanWEB <94617890+rainbowmanWEB@users.noreply.github.com>
2023-02-07 15:57:44 +0300

    block: header - add HTML
    Add HTML for block header

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import './scss/main.scss';
 import './js/main.js';
+import photo1 from './images/photo-1.jpg';
+import note from './images/icons/music-note-1.svg';
+import arrow from './images/arrow.png';
+import decorate from './images/stroke-2.png';
