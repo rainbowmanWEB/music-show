6ad30b6ae92111bd12368672a7ead0c30623e3dc
rainbowmanWEB <94617890+rainbowmanWEB@users.noreply.github.com>
2023-02-10 17:18:24 +0300

    block: header - add styles
    Add stylles for blocck header.

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ import photo1 from './images/photo-1.jpg';
 import note from './images/icons/music-note-1.svg';
 import arrow from './images/arrow.png';
 import decorate from './images/stroke-2.png';
+import headerBG from './images/header-bg.png';
+import stroke from './images/stroke.png';
